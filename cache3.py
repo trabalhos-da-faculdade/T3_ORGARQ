da292bcafe74b36202b7ec7fba90257067e6d0a6
Gabriel Fanto Stundner <gabriel.stundner@acad.pucrs.br>
2019-11-06 17:23:45 -0300

    cache3 almost done!

--- cache3.py
+++ cache3.py
@@ -25,6 +25,8 @@ cacheCount = 0
 # to check if cache memory is full
 isFull = 16
 
+# to insert the min value of count inside Linha
+minValue = -1
 # Reading the binary file
 
 for line in f:
@@ -32,27 +34,41 @@ for line in f:
     tag = line[0:13]
     palavra = line[14:16]
     byte = line[15:16]
-    # print("tag: " + tag)
-    # print("palavras: " + palavras)
-    # print("byte: " + byte)
+    print("tag: " + tag),
+    print("palavras: " + palavra),
+    print("byte: " + byte)
 
-    if cache[cacheCount] == None:
+    if cache[cacheCount].tag == "":
         cache[cacheCount].palavra = line
         cache[cacheCount].tag = tag
         print("Miss;" + str(cache[cacheCount].palavra))
+        miss = miss + 1
         cacheCount = cacheCount + 1
     else:
-        if cache[cacheCount].palavra == tag:
-            print("Hit;" + str(cache[cacheCount].palavra))
-            cache[cacheCount].count = cache[cacheCount].count + 1
-            cacheCount = cacheCount + 1
-        else:
-            if cacheCount == isFull:
+        if cacheCount == isFull:
                 cacheCount = 0
+        
+        else:
+            if cache[cacheCount].palavra == tag:
+                print("Hit;" + str(cache[cacheCount].palavra))
+                hit = hit + 1
+                cache[cacheCount].count = cache[cacheCount].count + 1
+                if minValue > cache[cacheCount].count:
+                    minValue = cache[cacheCount].count
+                    cacheCount = cacheCount + 1
                 
             else:
-                if 
+                if cache[cacheCount].count < minValue and cache[cacheCount] != None:
+                    cache[cacheCount].tag = tag
+                    cache[cacheCount].palavra = palavra
+                    cache[cache].count  = -1
+                    print("Miss;" + str(cache[cacheCount].palavra))
+                    miss = miss + 1
+# TODO make this array start again
 
 
 
-
+print("Hits;" + str(hit))
+print("Misses;" + str(miss)) 
+print("Taxa de hits;" + str(round((float(hit) / (hit+miss))*10000) / 100) + "%")
+print("Taxa de misses;" + str(round((float(miss) / (hit+miss))*10000) / 100) + "%")
