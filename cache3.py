44984d47a5bc9fd12a20576dcfe83e784f0d3305
F4NT0 <gabriel.stundner@acad.pucrs.br>
2019-11-05 22:25:07 -0300

    cache 3 tested

--- cache3.py
+++ cache3.py
@@ -4,13 +4,18 @@ class Linha:
     byte = 0
     tag = ""
 
+class CounterTag:
+    tag = ""
+    counter = -1
+
 # Reading file
 f = open("binary.txt","r")
 
 
 
 cache = [None] * 16
-memory = [None] * 264
+memory = [None] * 16
+testing = [None] * 16
 
 # Variaveis hit e miss 
 hit = 0
@@ -22,10 +27,16 @@ count = 0
 # Counter to change the value from position
 countToChange = 0
 
+# Bigger value to change
+minValue = 1000000
+
 
 # Cada posicao do cache possui a inicializacao da classe Linha
 for x in range (0,16):
     cache[x] = Linha()
+
+for y in range (len(memory)):
+    testing[y] = CounterTag()
 
 for line in f:
 
@@ -37,21 +48,29 @@ for line in f:
         memory[count] = tag
         print("Miss: " + str(int(line,2)))
         miss = miss + 1
+        testing[count].tag = tag
         count = count + 1
-    else if memory[count] == tag :
-        print("Hit: " + str(int(line,2)))
-        hit = hit + 1
-        count = count + 1
-    else if 
-    
-    
+    else:
+        if memory[count] == tag :
+            print("Hit: " + str(int(line,2)))
+            hit = hit + 1
+            count = count + 1
+
+            for i in range(len(testing)):
+                if testing[i].tag == tag:
+                    testing[i].counter = testing[i].counter + 1
+                    if testing[i].counter < minValue:
+                        minValue = testing[i].counter
+
+        else:
+            if count == len(memory):
+                for i in range(len(testing)):
+                    if testing[i].counter == minValue:
+                        print("Miss: " + str(int(line,2)))
+                        testing[i].tag = tag
 
 
-
-# print("Hits: " + str(hit))
-# print("Misses: " + str(miss)) 
-# print("Taxa de hits: " + str(round((float(hit) / (hit+miss))*10000) / 100) + "%")
-# print("Taxa de misses: " + str(round((float(miss) / (hit+miss))*10000) / 100) + "%")
-
-for i in range(0,263):
-    print(memory[i])
+print("Hits: " + str(hit))
+print("Misses: " + str(miss)) 
+print("Taxa de hits: " + str(round((float(hit) / (hit+miss))*10000) / 100) + "%")
+print("Taxa de misses: " + str(round((float(miss) / (hit+miss))*10000) / 100) + "%")
